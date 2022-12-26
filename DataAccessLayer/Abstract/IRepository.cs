07f18672c5a96f711f31e557ceba6abdbf97c4ec
Muhammed Yusuf ALTUNÇ <96544170+YusufAltunc@users.noreply.github.com>
2022-12-26 15:39:04 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    //T ne ise o tabloyu getircek bize yanı her tablo icin ayrı bir interface acmaya gerek yok
+    public interface IRepository<T>
+    {
+        List<T> List();
+
+        void Insert(T p);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Delete(T p);
+
+        void Update(T p);
+
+        List<T> List(Expression<Func<T, bool>> filter);
+    }
+}
